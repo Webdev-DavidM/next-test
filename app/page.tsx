bb3440c90d722ca7140defa733a068de52c38dbc
Webdev-DavidM <footballblubber@googlemail.com>
2024-01-31 13:16:30 +0000

    husky test commit

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,6 @@ import { Welcome } from '../components/Welcome/Welcome';
 import { ColorSchemeToggle } from '../components/ColorSchemeToggle/ColorSchemeToggle';
 
 export default function HomePage() {
-  console.log('HomePage');
   return (
     <>
       <Welcome />
